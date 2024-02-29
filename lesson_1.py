0bfcb7beb2fa5d446d1024cc5c87e930e67848b5
walker3354 <119646593+walker3354@users.noreply.github.com>
2024-02-29 15:58:57 +0800

    add lesson 1 basic concept

--- lesson_1.py
+++ lesson_1.py
@@ -1,1 +1,12 @@
-print('test')
+age_str = '21'  #"" same as '' in python
+print("this variable is", type(age_str))
+print(age_str)
+
+age_int = 21
+print('this variable is', type(age_int))
+print(age_int + 1)
+
+#translate type
+
+print(int(age_str) + age_int)
+print(str(age_int) + age_str)
