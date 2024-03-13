c088cf26568ccea0733e074dacfd4a1c33c89e01
walker3354 <119646593+walker3354@users.noreply.github.com>
2024-03-13 19:54:53 +0800

    add flask new course lesson example

--- flask/practice_1.py
+++ flask/practice_1.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home_page')
 def index():
     paragraphs = ["section 1", "section 2", "section 3"]
     return render_template("index.html", title="Home page", data=paragraphs)
+
+
+@app.route('/about_page')
+def about():
+    return "<h1>About page</h1>"
 
 
 if __name__ == '__main__':
