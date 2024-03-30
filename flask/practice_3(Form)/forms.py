bc44c8c010fc8be6c16cbea03363412f51a71a99
walker3354 <119646593+walker3354@users.noreply.github.com>
2024-03-30 09:10:09 +0800

    add lesson example

---
+++ flask/practice_3(Form)/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+# note DataRequired() will make sure the input wont be empty
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        'Confirm_Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remeber Me')
+    submit = SubmitField('Login')
